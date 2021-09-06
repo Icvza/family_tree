266c4fc82971cb662ec91b5f05e5c14017123fe7
Isaac <deisaacalejandro@gmail.com>
2021-09-05 20:06:09 -0400

    workstation move

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Member.create(first_name:'Isaac', last_name: 'chavez', age:25)
+Member.create(first_name:'Maria', last_name: 'colon', age:30)
+Member.create(first_name:'Lucianio', last_name: 'sinclair', age:1)
